fe1d94349ef9a680b418b9f6b33908e2aa775fde
Dominik Kocka <Dominik.Kocka@fodmobilitygroup.com>
2023-03-18 13:52:01 +0100

    Add backend functionality

--- WebService_FIBRPN/Cinema.cs
+++ WebService_FIBRPN/Cinema.cs
@@ -1,42 +1,124 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using System.ServiceModel;
+using System;
+using System.Reflection;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SeatReservation
 {
     public class Cinema : ICinema
     {
-        public Task BuyAsync(string lockId)
+        private static CinemaRoom? room;
+        private static FaultException<CinemaException> RoomUninitializedException = new FaultException<CinemaException>(
+               new CinemaException() {
+                   ErrorCode = 99,
+                   ErrorMessage = "Cinema room is not yet initialized"
+               }
+            );
+
+        public async Task InitAsync(int rows, int columns)
         {
-            throw new NotImplementedException();
+            if ((1 <= rows && rows <= 26) && (1 <= columns && columns <= 100))
+            {
+                room = new CinemaRoom(rows, columns);
+            }
+            else
+            {
+                CinemaException exception = new CinemaException();
+                exception.ErrorCode = 1;
+                exception.ErrorMessage = "Cannot create room without seats or bigger than 26 rows or 100 columns.";
+                Exception e = new Exception();
+                throw new FaultException<CinemaException>(exception);
+            }
         }
 
-        public Task<Seat[]> GetAllSeatsAsync()
+        public async Task<Seat[]> GetAllSeatsAsync()
         {
-            throw new NotImplementedException();
+            if (room is not null) return room.GetAllSeats().ToArray();
+
+            else return new List<Seat>().ToArray();
         }
 
-        public Task<SeatStatus> GetSeatStatusAsync(Seat seat)
+        public async Task<SeatStatus> GetSeatStatusAsync(Seat seat)
         {
-            throw new NotImplementedException();
+            if (room is null) throw RoomUninitializedException;
+
+            SeatStatus? tempSeatStatus = room.GetSeatStatus(seat);
+
+            if (tempSeatStatus is SeatStatus value) return value;
+            else
+            {
+                CinemaException exception = new CinemaException();
+                exception.ErrorCode = 2;
+                exception.ErrorMessage = "Unable to find seat with given id.";
+                Exception e = new Exception();
+                throw new FaultException<CinemaException>(exception);
+            }
         }
 
-        public Task InitAsync(int rows, int columns)
+        public async Task<string> LockAsync(Seat seat, int count)
         {
-            throw new NotImplementedException();
+            if (room is null) throw RoomUninitializedException;
+
+            string? lockId = room.LockSeats(seat, count);
+
+            if (lockId is not null) return lockId;
+            else
+            {
+                CinemaException exception = new CinemaException();
+                exception.ErrorCode = 3;
+                exception.ErrorMessage = "Unable to lock seats from given id.";
+                Exception e = new Exception();
+                throw new FaultException<CinemaException>(exception);
+            }
         }
 
-        public Task<string> LockAsync(Seat seat, int count)
+        public async Task UnlockAsync(string lockId)
         {
-            throw new NotImplementedException();
+            if (room is null) throw RoomUninitializedException;
+
+            bool success = room.UnlockSeats(lockId);
+            if (!success)
+            {
+                CinemaException exception = new CinemaException();
+                exception.ErrorCode = 4;
+                exception.ErrorMessage = "Unable to unlock seats with given lockid.";
+                Exception e = new Exception();
+                throw new FaultException<CinemaException>(exception);
+            }
         }
 
-        public Task ReserveAsync(string lockId)
+        public async Task ReserveAsync(string lockId)
         {
-            throw new NotImplementedException();
+            if (room is null) throw RoomUninitializedException;
+
+            bool success = room.ReserveSeats(lockId);
+            if (!success)
+            {
+                CinemaException exception = new CinemaException();
+                exception.ErrorCode = 5;
+                exception.ErrorMessage = "Unable to reserve seats with given lockid.";
+                Exception e = new Exception();
+                throw new FaultException<CinemaException>(exception);
+            }
         }
 
-        public Task UnlockAsync(string lockId)
+        public async Task BuyAsync(string lockId)
         {
-            throw new NotImplementedException();
+            if (room is null) throw RoomUninitializedException;
+
+            bool success = room.BuySeats(lockId);
+            if (!success)
+            {
+                CinemaException exception = new CinemaException();
+                exception.ErrorCode = 6;
+                exception.ErrorMessage = "Unable to buy seats with given lockid.";
+                Exception e = new Exception();
+                throw new FaultException<CinemaException>(exception);
+            }
         }
+
     }
 }
---
+++ WebService_FIBRPN/CinemaRoom.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace SeatReservation
+{
+    public class CinemaRoom
+    {
+        public class CinemaRow
+        {
+            public class CinemaSeat
+            {
+                public int ColumnNumber;
+                public SeatStatus seatStatus;
+                public string? Lockid;
+
+                public CinemaSeat(int columnNumber)
+                {
+                    this.ColumnNumber = columnNumber;
+                    this.seatStatus = SeatStatus.Free;
+                    this.Lockid = null;
+                }
+            }
+            private int RowNumber;
+            public char RowId
+            {
+                get
+                {
+                    return RowIdFromNumber(RowNumber);
+                }
+                set
+                {
+                    RowNumber = RowNumberFromId(value);
+                }
+            }
+            public List<CinemaSeat> seats;
+
+            public CinemaRow(int rowNumber, int columns)
+            {
+                this.RowNumber = rowNumber;
+                seats = new List<CinemaSeat>();
+                for (int i = 1; i <= columns; i++)
+                {
+                    this.seats.Add(new CinemaSeat(i));
+                }
+            }
+
+            public char RowIdFromNumber(int number)
+            {
+                return (char)(number + 64);
+            }
+
+            public int RowNumberFromId(char id)
+            {
+                return char.ToUpper(id) - 64;
+            }
+
+            public Seat[] GetAllSeats()
+            {
+                List<Seat> tempSeats = new List<Seat>();
+                foreach (var seat in seats)
+                {
+                    Seat tempSeat = new Seat();
+                    tempSeat.Row = this.RowId.ToString();
+                    tempSeat.Column = seat.ColumnNumber.ToString();
+                    tempSeats.Add(tempSeat);
+                }
+                return tempSeats.ToArray();
+            }
+
+            public string? LockSeats(int startingSeatNo, int count)
+            {
+                if (startingSeatNo < 1 || count < 1) return null;
+                if (seats.Count - (startingSeatNo - 1) <= count) return null;
+
+                CinemaSeat firstSeat = seats.Find(seat => seat.ColumnNumber == startingSeatNo);
+
+                if (firstSeat is not null)
+                {
+                    int firstIdx = seats.IndexOf(firstSeat);
+                    var tempSeats = seats.GetRange(firstIdx, count);
+                    foreach (var tempSeat in tempSeats)
+                    {
+                        if (tempSeat.seatStatus is not SeatStatus.Free) return null;
+                    }
+                    string lockId = Guid.NewGuid().ToString();
+                    foreach (var tempSeat in tempSeats)
+                    {
+                        tempSeat.seatStatus = SeatStatus.Locked;
+                        tempSeat.Lockid = lockId;
+                    }
+                    return lockId;
+                }
+                return null;
+            }
+        }
+
+        public List<CinemaRow> cinemaRows;
+
+        public CinemaRoom(int rows, int columns)
+        {
+            this.cinemaRows = new List<CinemaRow>();
+            for (int i = 1; i <= rows; i++)
+            {
+                this.cinemaRows.Add(new CinemaRow(i, columns));
+            }
+        }
+
+        public List<Seat> GetAllSeats()
+        {
+            List<Seat> tempSeats = new List<Seat>();
+            foreach (var row in cinemaRows)
+            {
+                tempSeats.AddRange(row.GetAllSeats());
+            }
+            return tempSeats;
+        }
+
+        public SeatStatus? GetSeatStatus(Seat seat)
+        {
+            SeatStatus? tempSeatStatus = null;
+            CinemaRow tempRow = this.cinemaRows.Find(row => row.RowId == seat.Row[0]);
+            if (tempRow is not null)
+            {
+                CinemaRow.CinemaSeat tempSeat = tempRow.seats.Find(column => column.ColumnNumber.ToString() == seat.Column);
+                if (tempSeat is not null) tempSeatStatus = tempSeat.seatStatus;
+            }
+            return tempSeatStatus;
+        }
+
+        public string? LockSeats(Seat startingSeat, int count)
+        {
+            CinemaRow tempRow = this.cinemaRows.Find(row => row.RowId == startingSeat.Row[0]);
+            if (tempRow is not null)
+            {
+                return tempRow.LockSeats(int.Parse(startingSeat.Column), count);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public bool UnlockSeats(string lockId)
+        {
+            bool isSuccessful = false;
+            foreach (var cinemaRow in cinemaRows)
+            {
+                foreach (var cinemaSeat in cinemaRow.seats)
+                {
+                    if (cinemaSeat.Lockid == lockId)
+                    {
+                        if (cinemaSeat.seatStatus is not SeatStatus.Locked) return false;
+                        isSuccessful = true;
+                        cinemaSeat.Lockid = null;
+                        cinemaSeat.seatStatus = SeatStatus.Free;
+                    }
+                }
+            }
+            return isSuccessful;
+        }
+
+        public bool ReserveSeats(string lockId)
+        {
+            bool isSuccessful = false;
+            foreach (var cinemaRow in cinemaRows)
+            {
+                foreach (var cinemaSeat in cinemaRow.seats)
+                {
+                    if (cinemaSeat.Lockid == lockId)
+                    {
+                        if (cinemaSeat.seatStatus is not SeatStatus.Locked) return false;
+                        isSuccessful = true;
+                        cinemaSeat.seatStatus = SeatStatus.Reserved;
+                    }
+                }
+            }
+            return isSuccessful;
+        }
+
+        public bool BuySeats(string lockId)
+        {
+            bool isSuccessful = false;
+            foreach (var cinemaRow in cinemaRows)
+            {
+                foreach (var cinemaSeat in cinemaRow.seats)
+                {
+                    if (cinemaSeat.Lockid == lockId)
+                    {
+                        if (cinemaSeat.seatStatus is not SeatStatus.Locked && cinemaSeat.seatStatus is not SeatStatus.Reserved) return false;
+                        isSuccessful = true;
+                        cinemaSeat.seatStatus = SeatStatus.Sold;
+                    }
+                }
+            }
+            return isSuccessful;
+        }
+    }
+}
+
